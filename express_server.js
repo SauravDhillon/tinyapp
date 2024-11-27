dcebc955a12c4d70e29575365443383173bbc8df
Saurav Dhillon <souravdhillon010@gmail.com>
2024-11-27 17:36:47 -0500

    Updated express_server.js file to handle form submission and added redirect link to the shortURL

--- express_server.js
+++ express_server.js
@@ -8,6 +8,7 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+//Take a form string and convert it into object(req.body)
 app.use(express.urlencoded({extended: true}));
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -29,14 +30,19 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok");
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  // console.log(urlDatabase);
+  // console.log(req.body); // Log the POST request body to the console
+  //res.send("Ok");
+  res.redirect(`/urls/${shortURL}`);
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tiny app listening on port ${PORT}!`);
 });
 
 function generateRandomString() {
