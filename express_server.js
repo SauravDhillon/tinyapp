fa833d25b3a00fe7bfb96b5836135ac48a95876c
Saurav Dhillon <souravdhillon010@gmail.com>
2024-11-27 02:31:09 -0500

    Changes were made to express_server.js file

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
+app.use(express.urlencoded({extended: true}));
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -16,6 +16,9 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = urlDatabase[id];
@@ -25,9 +28,17 @@ app.get("/urls/:id", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok");
+});
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  return Math.random().toString(36).slice(2, 8);
+};
